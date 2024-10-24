d512a97c945de92bfcad8c4b0f0edf26d077612a
NilmarLutino <lutinonilmar@gmail.com>
2024-10-24 18:55:43 -0400

    consumo externo

---
+++ frontend-gastos/services/apiConfig.ts
@@ -0,0 +1,5 @@
+// src/config/apiConfig.ts
+const LOCAL_BACKEND_IP = "100.127.255.172";
+const BACKEND_PORT = 3000;
+
+export const API_BASE_URL = `http://${LOCAL_BACKEND_IP}:${BACKEND_PORT}`;
--- frontend-gastos/services/eventService.ts
+++ frontend-gastos/services/eventService.ts
@@ -1,3 +1,5 @@
+import { API_BASE_URL } from "../services/apiConfig";
+
 // services/eventsService.ts
 export type CreateEventPayload = {
   nombre_evento: string;
@@ -7,246 +9,279 @@ export type CreateEventPayload = {
 };
 
 // Función para crear un evento
-export const createEvent = async (eventData: CreateEventPayload): Promise<void> => {
-try {
-  const response = await fetch("http://localhost:3000/api/eventos/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify(eventData),
-  });
+export const createEvent = async (
+  eventData: CreateEventPayload
+): Promise<void> => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/eventos/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(eventData),
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  if (!response.ok) {
-    throw new Error(data.message || "Error creating event.");
+    if (!response.ok) {
+      throw new Error(data.message || "Error creating event.");
+    }
+
+    console.log("Evento creado correctamente:", data);
+  } catch (error) {
+    console.error("Error al crear el evento:", error);
+    throw error;
   }
-
-  console.log("Evento creado correctamente:", data);
-} catch (error) {
-  console.error("Error al crear el evento:", error);
-  throw error; // Re-lanza el error para manejarlo donde se use esta función
-}
 };
 
 export const fetchUserEvents = async (userId: number) => {
-//export const fetchUserEvents = async (userId: number) => {
-    try {
-      const response = await fetch(`http://localhost:3000/api/eventos/summary-user/${userId}`, {
+  //export const fetchUserEvents = async (userId: number) => {
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}/api/eventos/summary-user/${userId}`,
+      {
         method: "GET",
         headers: {
           Accept: "application/json",
         },
-      });
-  
-      if (!response.ok) {
-        throw new Error("Error fetching user events");
       }
-  
-      const json = await response.json();
-      return json.result; // Retorna la lista de eventos
-    } catch (error) {
-      console.error("Error fetching user events:", error);
-      throw error; // Re-lanza el error para manejarlo donde se use esta función
-    }
-  };
-  
-  export const fetchEventDetails = async (eventId: string) => {
-    try {
-      const response = await fetch(`http://localhost:3000/api/eventos/${eventId}`, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error fetching details for event ID: ${eventId}`);
-      }
-  
-      const json = await response.json();
-      return {
-        fecha: json.result.fecha,
-        creado_por: json.result.creado_por,
-      }; // Retorna un objeto con la fecha y el creador
-    } catch (error) {
-      console.error(`Error fetching details for event ID ${eventId}:`, error);
-      throw error; // Re-lanza el error para manejarlo donde se use esta función
-    }
-  };
-  
+    );
 
-  export const fetchEventSummary = async (groupId: string, userId: number) => {
-    try {
-      const response = await fetch(
-        `http://localhost:3000/api/eventos/summary/${groupId}?userId=${userId}`,
-        {
-          method: "GET",
-          headers: {
-            Accept: "application/json",
-          },
-        }
+    if (!response.ok) {
+      throw new Error("Error fetching user events");
+    }
+
+    const json = await response.json();
+    return json.result; // Retorna la lista de eventos
+  } catch (error) {
+    console.error("Error fetching user events:", error);
+    throw error; // Re-lanza el error para manejarlo donde se use esta función
+  }
+};
+
+export const fetchEventDetails = async (eventId: string) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/eventos/${eventId}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error fetching details for event ID: ${eventId}`);
+    }
+
+    const json = await response.json();
+    return {
+      fecha: json.result.fecha,
+      creado_por: json.result.creado_por,
+    }; // Retorna un objeto con la fecha y el creador
+  } catch (error) {
+    console.error(`Error fetching details for event ID ${eventId}:`, error);
+    throw error; // Re-lanza el error para manejarlo donde se use esta función
+  }
+};
+
+export const fetchEventSummary = async (groupId: string, userId: number) => {
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}/api/eventos/summary/${groupId}?userId=${userId}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error fetching event summary for ID: ${groupId}`);
+    }
+
+    const json = await response.json();
+    return json.result; // Retorna el resumen del evento
+  } catch (error) {
+    console.error(`Error fetching event summary for ID ${groupId}:`, error);
+    throw error; // Re-lanza el error para manejarlo donde se use esta función
+  }
+};
+
+export const fetchEventParticipants = async (groupId: string) => {
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}/api/participantes/event/${groupId}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error fetching participants for event ID: ${groupId}`);
+    }
+
+    const json = await response.json();
+    return json.result; // Retorna la lista de participantes
+  } catch (error) {
+    console.error(
+      `Error fetching participants for event ID ${groupId}:`,
+      error
+    );
+    throw error;
+  }
+};
+
+export const fetchEventParticipantExpenses = async (
+  participantId: string,
+  groupId: string
+) => {
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}/api/gastos/participante/${participantId}/evento/${groupId}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Error fetching expenses for participant ID: ${participantId} and event ID: ${groupId}`
       );
-  
-      if (!response.ok) {
-        throw new Error(`Error fetching event summary for ID: ${groupId}`);
-      }
-  
-      const json = await response.json();
-      return json.result; // Retorna el resumen del evento
-    } catch (error) {
-      console.error(`Error fetching event summary for ID ${groupId}:`, error);
-      throw error; // Re-lanza el error para manejarlo donde se use esta función
     }
-  };
-  
-  export const fetchEventParticipants = async (groupId: string) => {
-    try {
-      const response = await fetch(`http://localhost:3000/api/participantes/event/${groupId}`, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error fetching participants for event ID: ${groupId}`);
-      }
-  
-      const json = await response.json();
-      return json.result; // Retorna la lista de participantes
-    } catch (error) {
-      console.error(`Error fetching participants for event ID ${groupId}:`, error);
-      throw error;
-    }
-  };
-  
-  export const fetchEventParticipantExpenses = async (participantId: string, groupId: string) => {
-    try {
-      const response = await fetch(`http://localhost:3000/api/gastos/participante/${participantId}/evento/${groupId}`, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error fetching expenses for participant ID: ${participantId} and event ID: ${groupId}`);
-      }
-  
-      const json = await response.json();
-      return json.result; // Retorna la lista de gastos
-    } catch (error) {
-      console.error(`Error fetching expenses for participant ID ${participantId} and event ID ${groupId}:`, error);
-      throw error;
-    }
-  };
-  
-  export const createExpense = async (
-    eventoId: number,
-    monto: number,
-    categoria: string,
-    descripcion: string,
-    agregadoPor: number
-  ) => {
-    try {
-      const response = await fetch("http://localhost:3000/api/gastos/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify({
-          evento_id: eventoId,
-          monto,
-          categoria,
-          descripcion,
-          fecha: new Date().toISOString().split("T")[0], // Fecha actual en formato YYYY-MM-DD
-          agregado_por: agregadoPor,
-        }),
-      });
-  
-      if (!response.ok) {
-        throw new Error("Error al crear el gasto");
-      }
-  
-      const json = await response.json();
-      return json.result; // Devuelve el resultado de la creación del gasto
-    } catch (error) {
-      console.error("Error al crear el gasto:", error);
-      throw error;
-    }
-  };
 
-  export const fetchUserByEmail = async (email: string) => {
-    try {
-      const response = await fetch(`http://localhost:3000/api/usuarios/email/${email}`, {
+    const json = await response.json();
+    return json.result; // Retorna la lista de gastos
+  } catch (error) {
+    console.error(
+      `Error fetching expenses for participant ID ${participantId} and event ID ${groupId}:`,
+      error
+    );
+    throw error;
+  }
+};
+
+export const createExpense = async (
+  eventoId: number,
+  monto: number,
+  categoria: string,
+  descripcion: string,
+  agregadoPor: number
+) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/gastos/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        evento_id: eventoId,
+        monto,
+        categoria,
+        descripcion,
+        fecha: new Date().toISOString().split("T")[0], // Fecha actual en formato YYYY-MM-DD
+        agregado_por: agregadoPor,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al crear el gasto");
+    }
+
+    const json = await response.json();
+    return json.result; // Devuelve el resultado de la creación del gasto
+  } catch (error) {
+    console.error("Error al crear el gasto:", error);
+    throw error;
+  }
+};
+
+export const fetchUserByEmail = async (email: string) => {
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}/api/usuarios/email/${email}`,
+      {
         method: "GET",
         headers: {
           Accept: "application/json",
         },
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error fetching user with email: ${email}`);
       }
-  
-      const json = await response.json();
-      return json.result;
-    } catch (error) {
-      console.error("Error fetching user:", error);
-      throw error;
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error fetching user with email: ${email}`);
     }
-  };
-  
-  export const addParticipant = async (eventoId: string, usuarioId: string, rolesId: number) => {
-    try {
-      const response = await fetch(`http://localhost:3000/api/participantes/`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify({
-          evento_id: eventoId,
-          usuario_id: usuarioId,
-          ha_pagado: false,
-          roles_id: rolesId,
-        }),
-      });
-  
-      if (!response.ok) {
-        throw new Error("Error al agregar el participante");
-      }
-  
-      const json = await response.json();
-      return json.result;
-    } catch (error) {
-      console.error("Error adding participant:", error);
-      throw error;
+
+    const json = await response.json();
+    return json.result;
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    throw error;
+  }
+};
+
+export const addParticipant = async (
+  eventoId: string,
+  usuarioId: string,
+  rolesId: number
+) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/participantes/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        evento_id: eventoId,
+        usuario_id: usuarioId,
+        ha_pagado: false,
+        roles_id: rolesId,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al agregar el participante");
     }
-  };
-  
-  export const fetchParticipantById = async (participantId: string) => {
-    try {
-      const response = await fetch(`http://localhost:3000/api/participantes/${participantId}`, {
+
+    const json = await response.json();
+    return json.result;
+  } catch (error) {
+    console.error("Error adding participant:", error);
+    throw error;
+  }
+};
+
+export const fetchParticipantById = async (participantId: string) => {
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}/api/participantes/${participantId}`,
+      {
         method: "GET",
         headers: {
           Accept: "application/json",
         },
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error fetching participant with ID: ${participantId}`);
       }
-  
-      const json = await response.json();
-      return json.result;
-    } catch (error) {
-      console.error(`Error fetching participant with ID ${participantId}:`, error);
-      throw error;
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error fetching participant with ID: ${participantId}`);
     }
-  };
-  
+
+    const json = await response.json();
+    return json.result;
+  } catch (error) {
+    console.error(
+      `Error fetching participant with ID ${participantId}:`,
+      error
+    );
+    throw error;
+  }
+};
