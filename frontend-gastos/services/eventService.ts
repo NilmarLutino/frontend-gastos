4136d7c1d7927601547b39f70aa9dd73542a39f2
alelanda135 <alejandra.landaeta@ucb.edu.bo>
2024-10-23 22:33:50 -0400

    funcion crear grupo

--- frontend-gastos/services/eventService.ts
+++ frontend-gastos/services/eventService.ts
@@ -1,4 +1,36 @@
 // services/eventsService.ts
+export type CreateEventPayload = {
+  nombre_evento: string;
+  fecha: string;
+  creado_por: number;
+  descripcion: string;
+};
+
+// Función para crear un evento
+export const createEvent = async (eventData: CreateEventPayload): Promise<void> => {
+try {
+  const response = await fetch("http://localhost:3000/api/eventos/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Error creating event.");
+  }
+
+  console.log("Evento creado correctamente:", data);
+} catch (error) {
+  console.error("Error al crear el evento:", error);
+  throw error; // Re-lanza el error para manejarlo donde se use esta función
+}
+};
+
 export const fetchUserEvents = async (userId: number) => {
 //export const fetchUserEvents = async (userId: number) => {
     try {
